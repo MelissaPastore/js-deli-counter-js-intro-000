9003913ae7c20ce3203631178106ed9d7cbe6b76
Melissa Pastore <melissa.pastore@gmail.com>
2020-06-26 19:50:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ function currentLine (currLine) {
     return 'The line is currently empty.'
   }
   let greeting = 'The line is currently: '
-   currLine.forEach((person, index) => greeting +=`${index+1}. ${currPerson}`)
-
+  for(let i=0; i<currLine.length; i++){
+    let currPerson = currLine[i];
+    if(i === currLine.length-1){
+      greeting += `${i+1}. ${currPerson}`
+    }
+    else {
+      greeting += `${i+1}. ${currPerson}, `
+    }
   return greeting; 
+  }
 }
